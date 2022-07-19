84f57aef2fcbc65eaefa02b0ee956ae6dd68ce47
Danielle Martin <ordinaryworld2014@gmail.com>
2022-07-19 17:17:44 -0500

    updated sql

--- kennel.sql
+++ kennel.sql
@@ -3,8 +3,6 @@ CREATE TABLE `Location` (
 	`name`	TEXT NOT NULL,
 	`address`	TEXT NOT NULL
 );
-INSERT INTO `Location` VALUES (null, 'Nashville North', "64 Washington Heights");
-INSERT INTO `Location` VALUES (null, 'Nashville South', "101 Penn Ave");
 
 CREATE TABLE `Customer` (
     `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -13,13 +11,6 @@ CREATE TABLE `Customer` (
     `email`    TEXT NOT NULL,
     `password`    TEXT NOT NULL
 );
-
-INSERT INTO `Customer` VALUES (null, "Mo Silvera", "201 Created St", "mo@silvera.com", "password");
-INSERT INTO `Customer` VALUES (null, "Bryan Nilsen", "500 Internal Error Blvd", "bryan@nilsen.com", "password");
-INSERT INTO `Customer` VALUES (null, "Jenna Solis", "301 Redirect Ave", "jenna@solis.com", "password");
-INSERT INTO `Customer` VALUES (null, "Emily Lemmon", "454 Mulberry Way", "emily@lemmon.com", "password");
-INSERT INTO `Customer` VALUES (null, "Kitty Purry", "568 Fluffytails Blvd", "kitty@purry.com", "password");
-INSERT INTO `Customer` VALUES (null, "Sakura Kidamora", "128 Coding Blvd", "SK1287@coding.com", "password");
 
 CREATE TABLE `Animal` (
 	`id`  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -32,6 +23,7 @@ CREATE TABLE `Animal` (
 	FOREIGN KEY(`location_id`) REFERENCES `Location`(`id`)
 );
 
+
 CREATE TABLE `Employee` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	`name`	TEXT NOT NULL,
@@ -41,29 +33,42 @@ CREATE TABLE `Employee` (
 
 );
 
-
+INSERT INTO `Location` VALUES (null, 'Nashville North', "64 Washington Heights");
+INSERT INTO `Location` VALUES (null, 'Nashville South', "101 Penn Ave");
 
 
 INSERT INTO `Employee` VALUES (null, "Dwight Schrute", "35498 Madison Ave", 1);
-INSERT INTO `Employee` VALUES (null, "Hisoka Morrow", "100 Main St", 1);
-INSERT INTO `Employee` VALUES (null, "Christina Miller", "204 Empty Ave", 1);
-INSERT INTO `Employee` VALUES (null, "Michael Scott", "200 Success Way", 2);
-INSERT INTO `Employee` VALUES (null, "Pam Beesly", "800 Wayside Way", 2);
-INSERT INTO `Employee` VALUES (null, "Illumi Zoldyck", "306 Internal Error Blvd", 2);
+INSERT INTO `Employee` VALUES (null, "Michael Scott", "100 Main St", 1);
+INSERT INTO `Employee` VALUES (null, "Angela Martin", "404 Unknown Ct", 2);
+INSERT INTO `Employee` VALUES (null, "Pam Beesly", "204 Empty Ave", 1);
+INSERT INTO `Employee` VALUES (null, "Jim Halpert", "200 Success Way", 2);
 
 
-
+INSERT INTO `Customer` VALUES (null, "Mo Silvera", "201 Created St", "mo@silvera.com", "password");
+INSERT INTO `Customer` VALUES (null, "Bryan Nilsen", "500 Internal Error Blvd", "bryan@nilsen.com", "password");
+INSERT INTO `Customer` VALUES (null, "Jenna Solis", "301 Redirect Ave", "jenna@solis.com", "password");
+INSERT INTO `Customer` VALUES (null, "Emily Lemmon", "454 Mulberry Way", "emily@lemmon.com", "password");
+INSERT INTO `Customer` VALUES (null, "Kitty Purry", "568 Fluffytails Blvd", "kitty@purry.com", "password");
+INSERT INTO `Customer` VALUES (null, "Sakura Kidamora", "128 Coding Blvd", "SK1287@coding.com", "password");
 
 
 
 INSERT INTO `Animal` VALUES (null, "Snickers", "Recreation", "Dalmation", 4, 1);
-INSERT INTO `Animal` VALUES (null, "Natsu", "Treatment", "Beagle", 1, 1);
-INSERT INTO `Animal` VALUES (null, "Fluffy", "Treatment", "Siamese", 4, 2);
-INSERT INTO `Animal` VALUES (null, "Pierre", "Kennel", "Poodle", 3, 1);
-INSERT INTO `Animal` VALUES (null, "Jasper", "Kennel", "Boxer", 2, 2);
-INSERT INTO `Animal` VALUES (null, "Hawthorne", "Kennel", "Chihuahua", 6, 2);
-INSERT INTO `Animal` VALUES (null, "Muffin", "Recreation", "Tabby", 5, 1);
-INSERT INTO `Animal` VALUES (null, "Daisy", "Recreation", "Calico", 5, 1);
+INSERT INTO `Animal` VALUES (null, "Jax", "Treatment", "Beagle", 1, 1);
+INSERT INTO `Animal` VALUES (null, "Falafel", "Treatment", "Siamese", 4, 2);
+INSERT INTO `Animal` VALUES (null, "Doodles", "Kennel", "Poodle", 3, 1);
+INSERT INTO `Animal` VALUES (null, "Daps", "Kennel", "Boxer", 2, 2);
+INSERT INTO `Animal` VALUES (null, "Hairy Paw-ter", "Recreation", "Dalmation", 4, 2);
+INSERT INTO `Animal` VALUES (null, "Arf Vader", "Treatment", "Beagle", 1, 2);
+INSERT INTO `Animal` VALUES (null, "Chewbarka", "Kennel", "Poodle", 3, 1);
+INSERT INTO `Animal` VALUES (null, "Droolius Caesar", "Kennel", "Boxer", 2, 2);
+INSERT INTO `Animal` VALUES (null, "Cat Benetar", "Recreation", "Tabby", 5, 1);
+INSERT INTO `Animal` VALUES (null, "Meownerva Meownagall", "Recreation", "Calico", 5, 1);
+INSERT INTO `Animal` VALUES (null, "Meowly Cyrus”,"Treatment", "Persian Cat", 6, 2);
+INSERT INTO `Animal` VALUES (null, "Fleas Witherspoon", "Kennel", "Persian Cat", 3, 1);
+INSERT INTO `Animal` VALUES (null, "Neil Catrick Harris", "Recreation", "Black Cat", 6, 2);
+INSERT INTO `Animal` VALUES (null, "JK Growling”, "Recreation", "Doxen-Chihuahua mix", 6, 2);
+
 -- Get only the animal rows where the `id` field value is 3
 SELECT
     a.id,
